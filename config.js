9c2fbca6ce61c0269cae1c2a8053c666a612c4d9
thaidv9989 <anhemm237@gmail.com>
2022-03-07 20:31:56 +0700

    em

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100018381218412';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc Em 08/03 tràn ngập niềm vui và hạnh phúc. Mong những điều đẹp nhất sẽ luôn đến bên em và luôn giữ nụ cười trên môi nhé     <3';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hienn2.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
